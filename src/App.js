0f88d9cb83c27a65ddf3920818e143ed7153d210
Ian McElroy <mcelroyian@gmail.com>
2020-04-14 14:50:03 -0500

    likes working

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ const App = () => {
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostsPage />
     </div>
   );
 };
