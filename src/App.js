49b61889e818dbfa889eb37c2d6f3cd70ab0ad95
Ian McElroy <mcelroyian@gmail.com>
2020-04-14 14:22:41 -0500

    first

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import SearchBar from "./components/SearchBar/SearchBarContainer"
+import PostsPage from "./components/PostsContainer/PostsPage"
 // import the PostsPage and SearchBar and add them to the App
 
 
