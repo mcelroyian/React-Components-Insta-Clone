dcda3f4229b58fd1f75e23f1ee51e38a6851cbb8
Ian McElroy <mcelroyian@gmail.com>
2020-04-14 16:28:46 -0500

    search works on username

--- src/App.js
+++ src/App.js
@@ -8,19 +8,33 @@ import React, { useState } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBarContainer"
 import PostsPage from "./components/PostsContainer/PostsPage"
+import instaPosts from './dummy-data'
 // import the PostsPage and SearchBar and add them to the App
 
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('')
+  const [posts, setPosts] = useState(instaPosts)
 
+  const filterPosts = () => {
+    if (searchTerm === '') {
+      return posts
+    }
+    let filteredPosts = posts.filter(post => {
+      if (post.username.includes(searchTerm)) {
+        return post
+      }
+    })
+    return filteredPosts
+  }
 
+ console.log(filterPosts())
   return (
     <div className="App">
       {/* Add imported components here to render them */}
       <SearchBar currentSearch={setSearchTerm}/>
-      <PostsPage />
+      <PostsPage posts={filterPosts()}/>
     </div>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,17 +3,17 @@
 import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-import instaPosts from '../../dummy-data'
+//import instaPosts from '../../dummy-data'
 // import data 
 
-const PostsPage = () => {
+const PostsPage = ({posts}) => {
   // set up state for your data
-  const [posts, setPosts] = useState(instaPosts)
+  //const [posts, setPosts] = useState(instaPosts)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
       {
-        posts.map( post => <Post post={post}/>)
+       (posts.length !== 0 ? posts.map( post => <Post post={post}/>) : <h2>Sorry, nothing matches your search</h2>)
       }
     </div>
   );
