2c596ddb26410e2b74ace0a70bcf0f90aaf6cab6
Ian McElroy <mcelroyian@gmail.com>
2020-04-15 09:15:40 -0500

    search wip

--- src/App.js
+++ src/App.js
@@ -17,24 +17,51 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [posts, setPosts] = useState(instaPosts)
 
+  //helper
+  const hasMatch = (str1, str2) => {
+    if (str1.toLowerCase().includes(str2.toLowerCase())) {
+      return true
+    } 
+    else return false
+  }
+
   const filterPosts = () => {
     if (searchTerm === '') {
       return posts
     }
+    let term = searchTerm.trim()
     let filteredPosts = posts.filter(post => {
-      if (post.username.includes(searchTerm)) {
-        return post
+      const postData = Object.values(post)
+
+      for (const value of postData) {
+        if (typeof value === "string") {
+        debugger
+          console.log('looked at string: ',value)
+          if (hasMatch(value, term)) {
+            
+            return post
+          } 
+        } if (Array.isArray(value)) {
+          console.log('looked at array: ', value)
+            for (const comment in value) {
+              if (hasMatch(value[comment].username, term) || hasMatch(value[comment].text, term)) {
+                
+                return post
+              } 
+            }
+        }
+        
       }
     })
     return filteredPosts
   }
 
- console.log(filterPosts())
+  //console.log(filterPosts())
   return (
     <div className="App">
       {/* Add imported components here to render them */}
-      <SearchBar currentSearch={setSearchTerm}/>
-      <PostsPage posts={filterPosts()}/>
+      <SearchBar currentSearch={setSearchTerm} />
+      <PostsPage posts={filterPosts()} />
     </div>
   );
 };
--- src/dummy-data.js
+++ src/dummy-data.js
@@ -44,6 +44,153 @@ const dummyData = [
                 text: "It that injustice"
             }
         ]
+    },
+    {
+      username: "Marylou Mann",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: "http://placehold.it/800",
+      likes: 2907,
+      timestamp: "2015-02-24T02:11:18 +06:00",
+      comments: [
+        {
+          username: "Dona Wilder",
+          text: "ad"
+        },
+        {
+          username: "Eugenia Valenzuela",
+          text: "ullamco"
+        },
+        {
+          username: "Deloris Ayers",
+          text: "est"
+        }
+      ]
+    },
+    {
+      username: "Esther Hester",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: "http://placehold.it/800",
+      likes: 3225,
+      timestamp: "2018-08-17T12:15:47 +05:00",
+      comments: [
+        {
+          username: "Ayala Oconnor",
+          text: "officia"
+        },
+        {
+          username: "Buckley Bowman",
+          text: "ea"
+        },
+        {
+          username: "Mcfadden Fitzgerald",
+          text: "nostrud"
+        }
+      ]
+    },
+    {
+      username: "Vincent Mckinney",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: "http://placehold.it/800",
+      likes: 513,
+      timestamp: "2019-11-29T11:04:38 +06:00",
+      comments: [
+        {
+          username: "Gabriela Daniels",
+          text: "officia"
+        },
+        {
+          username: "Lorna Herman",
+          text: "esse"
+        },
+        {
+          username: "Hollie Waters",
+          text: "officia"
+        }
+      ]
+    },
+    {
+      username: "Georgette Hyde",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: "http://placehold.it/800",
+      likes: 2716,
+      timestamp: "2016-07-17T10:51:14 +05:00",
+      comments: [
+        {
+          username: "Webb Ross",
+          text: "deserunt"
+        },
+        {
+          username: "Prince Meyer",
+          text: "ad"
+        },
+        {
+          username: "Sherman Rollins",
+          text: "et"
+        }
+      ]
+    },
+    {
+      username: "Pitts Everett",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: "http://placehold.it/800",
+      likes: 2329,
+      timestamp: "2020-02-17T06:53:03 +06:00",
+      comments: [
+        {
+          username: "Carter Woodward",
+          text: "laborum"
+        },
+        {
+          username: "Andrews Bowers",
+          text: "tempor"
+        },
+        {
+          username: "Potter Hudson",
+          text: "veniam"
+        }
+      ]
+    },
+    {
+      username: "Noel Garner",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: "http://placehold.it/800",
+      likes: 3832,
+      timestamp: "2014-02-26T05:34:46 +06:00",
+      comments: [
+        {
+          username: "Edna Velasquez",
+          text: "adipisicing"
+        },
+        {
+          username: "Serrano Horton",
+          text: "duis"
+        },
+        {
+          username: "Luann Anthony",
+          text: "quis"
+        }
+      ]
+    },
+    {
+      username: "Tabatha Combs",
+      thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+      imageUrl: "http://placehold.it/800",
+      likes: 1612,
+      timestamp: "2018-01-04T04:10:02 +06:00",
+      comments: [
+        {
+          username: "Adela Wilkerson",
+          text: "officia"
+        },
+        {
+          username: "Terrie Hutchinson",
+          text: "cupidatat"
+        },
+        {
+          username: "Robinson Mcgowan",
+          text: "esse"
+        }
+      ]
     }
 ];
 
